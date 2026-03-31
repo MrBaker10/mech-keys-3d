d44cad3f8b3b199ca27a3c238d23030f4a4ac58a
Sascha <baecker.sascha@googlemail.com>
2026-03-31 09:45:40 +0200

    chore: export KeyboardModel from three index

---
+++ src/components/three/index.ts
@@ -0,0 +1,4 @@
+export { Scene } from "./Scene";
+export { KeyboardModel } from "./KeyboardModel";
+export { KeyboardPlaceholder } from "./KeyboardPlaceholder";
+export { KeyboardExplode } from "./KeyboardExplode";
